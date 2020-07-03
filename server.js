9a2fc0c9f68983a5eb7e607230c99975ca60487b
Justin Le <justinqle@gmail.com>
2020-07-03 19:21:40 -0400

    cors, slight router modifcations

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const UMDCASStrategy = require('passport-umd-cas').Strategy;
+const cors = require('cors');
 
 passport.use(new UMDCASStrategy({ callbackURL: '/umd/return' }));
 
@@ -19,17 +20,21 @@ app.set('view engine', 'pug');
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(cors({credentials: true, origin: 'http://localhost:8080'}));
 
 app.get('/umd/login', passport.authenticate('umd-cas'));
 app.get('/umd/return', passport.authenticate('umd-cas'), (req, res) => {
-    res.redirect('/');
+    // res.redirect('/')
+    res.redirect('http://localhost:8080');
 });
 
 app.get('/', (req, res) => {
     if (req.user) {
-        res.render('profile', { title: 'Profile', user: req.user })
+        // res.render('profile', { title: 'Profile', user: req.user })
+        res.send(req.user);
     } else {
-        res.render('login', { title: 'Login' })
+        // res.render('login', { title: 'Login' })
+        res.send(null);
     }
 });
 
