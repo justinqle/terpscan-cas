2a80c50a149c7ec3788d86665ac3e2c211e3cac6
Justin Le <justinqle@gmail.com>
2020-07-06 19:51:44 -0400

    Prettier format

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 Backend for terpscan-web to authenticate with [UMD CAS](https://umd.service-now.com/kb_view.do?sysparm_article=KB0013650). Built on [Express](https://expressjs.com) and [Passport](http://www.passportjs.org).
 
 ## Setup
+
     npm install
     npm start
--- server.js
+++ server.js
@@ -1,41 +1,47 @@
-const express = require('express');
-const passport = require('passport');
-const UMDCASStrategy = require('passport-umd-cas').Strategy;
-const cors = require('cors');
+const express = require("express");
+const passport = require("passport");
+const UMDCASStrategy = require("passport-umd-cas").Strategy;
+const cors = require("cors");
 
-passport.use(new UMDCASStrategy({ callbackURL: '/umd/return' }));
+passport.use(new UMDCASStrategy({ callbackURL: "/umd/return" }));
 
 passport.serializeUser(function (user, done) {
-    done(null, user.uid);
+  done(null, user.uid);
 });
 
 passport.deserializeUser(function (obj, done) {
-    done(null, obj);
+  done(null, obj);
 });
 
 const app = express();
 
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(
+  require("express-session")({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(cors({credentials: true, origin: 'http://localhost:8080'}));
+app.use(cors({ credentials: true, origin: "http://localhost:8080" }));
 
-app.get('/umd/login', passport.authenticate('umd-cas'));
-app.get('/umd/return', passport.authenticate('umd-cas'), (req, res) => {
-    res.redirect('http://localhost:8080');
+app.get("/umd/login", passport.authenticate("umd-cas"));
+app.get("/umd/return", passport.authenticate("umd-cas"), (req, res) => {
+  res.redirect("http://localhost:8080");
 });
 
-app.get('/', (req, res) => {
-    if (req.user) {
-        res.send(req.user);
-    } else {
-        res.send(null);
-    }
+app.get("/", (req, res) => {
+  if (req.user) {
+    res.send(req.user);
+  } else {
+    res.send(null);
+  }
 });
 
-app.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('https://shib.idm.umd.edu/shibboleth-idp/profile/cas/logout');
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("https://shib.idm.umd.edu/shibboleth-idp/profile/cas/logout");
 });
 
-app.listen(3000, () => console.log('Starting server on 3000...'));
+app.listen(3000, () => console.log("Starting server on 3000..."));
